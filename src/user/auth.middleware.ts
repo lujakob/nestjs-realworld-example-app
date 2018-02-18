a01c3ad9242f225111911323b3bbb94d9031edae
Lukas Jakob <lu.jakob@googlemail.com>
2018-02-18 12:58:50 +0100

    refactor: Create user validation

--- src/user/auth.middleware.ts
+++ src/user/auth.middleware.ts
@@ -1,5 +1,5 @@
 import { HttpException } from '@nestjs/core';
-import { Middleware, NestMiddleware } from '@nestjs/common';
+import { Middleware, NestMiddleware, HttpStatus } from '@nestjs/common';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
@@ -16,14 +16,14 @@ export class AuthMiddleware implements NestMiddleware {
       if (req.headers.authorization && (req.headers.authorization as string).split(' ')[0] === 'Token') {
         const token = (req.headers.authorization as string).split(' ')[1];
         const decoded: any = jwt.verify(token, SECRET);
-        const user = await this.userService.findByEmail(decoded.email);
+        const user = await this.userService.findById(decoded.id);
 
         if (!user) {
-          throw new HttpException('User not found.', 401);
+          throw new HttpException('User not found.', HttpStatus.UNAUTHORIZED);
         }
         next();
       } else {
-        throw new HttpException('Not authorized.', 401);
+        throw new HttpException('Not authorized.', HttpStatus.UNAUTHORIZED);
       }
     };
   }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,5 +1,7 @@
 import {Entity, PrimaryGeneratedColumn, Column, BeforeInsert} from "typeorm";
+import {IsEmail, Validate} from "class-validator";
 import * as crypto from 'crypto';
+import { CustomEmail } from './CustomEmail';
 
 @Entity()
 export class User {
@@ -11,6 +13,7 @@ export class User {
   username: string;
 
   @Column()
+  @IsEmail()
   email: string;
 
   @Column({default: ''})
