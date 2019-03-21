81a68a68506223912fb49cf8defd4f4a36708534
arbassic <arbassic@gmail.com>
2019-03-21 20:27:26 +0100

    Fix the tag.controller test - provide more detailed TS mock implementation

--- src/tag/tag.controller.spec.ts
+++ src/tag/tag.controller.spec.ts
@@ -21,10 +21,20 @@ describe('TagController', () => {
 
   describe('findAll', () => {
     it('should return an array of tags', async () => {
-      const result = ['angularjs', 'reactjs'];
-      jest.spyOn(tagService, 'findAll').mockImplementation(() => result);
+      const tags : TagEntity[] = [];
+      const createTag = (id, name) => {
+        const tag = new TagEntity();
+        tag.id = id;
+        tag.tag = name;
+        return tag;
+      }
+      tags.push(createTag(1, 'angularjs'));
+      tags.push(createTag(2, 'reactjs'));
 
-      expect(await tagController.findAll()).toBe(result);
+      jest.spyOn(tagService, 'findAll').mockImplementation(() => Promise.resolve(tags));
+      
+      const findAllResult = await tagController.findAll();
+      expect(findAllResult).toBe(tags);
     });
   });
 });
