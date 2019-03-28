492bfd0bde203e4a56560c171d766885e4c0141a
Lukas Jakob <lu.jakob@googlemail.com>
2019-03-28 14:13:45 +0100

    Fix auth middleware

--- src/user/auth.middleware.ts
+++ src/user/auth.middleware.ts
@@ -10,25 +10,22 @@ import { UserService } from './user.service';
 export class AuthMiddleware implements NestMiddleware {
   constructor(private readonly userService: UserService) {}
 
-  use(): (req: Request, res: Response, next: NextFunction) => void {
+  async use(req: Request, res: Response, next: NextFunction) {
+    const authHeaders = req.headers.authorization;
+    if (authHeaders && (authHeaders as string).split(' ')[1]) {
+      const token = (authHeaders as string).split(' ')[1];
+      const decoded: any = jwt.verify(token, SECRET);
+      const user = await this.userService.findById(decoded.id);
 
-    return async (req: Request, res: Response, next: NextFunction) => {
-      if (req.headers.authorization && (req.headers.authorization as string).split(' ')[0] === 'Token') {
-        const token = (req.headers.authorization as string).split(' ')[1];
-        const decoded: any = jwt.verify(token, SECRET);
-        const user = await this.userService.findById(decoded.id);
-
-        if (!user) {
-          throw new HttpException('User not found.', HttpStatus.UNAUTHORIZED);
-        }
-
-        req.user = user.user;
-        next();
-
-      } else {
-        throw new HttpException('Not authorized.', HttpStatus.UNAUTHORIZED);
-
+      if (!user) {
+        throw new HttpException('User not found.', HttpStatus.UNAUTHORIZED);
       }
-    };
+
+      req.user = user.user;
+      next();
+
+    } else {
+      throw new HttpException('Not authorized.', HttpStatus.UNAUTHORIZED);
+    }
   }
 }
