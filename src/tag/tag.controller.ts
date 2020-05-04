db5d51890aea8a30314755d9aeefd50405be14f3
Han <xsytby1112@gmail.com>
2020-05-05 01:26:57 +0800

    feat: tags swagger and dto

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,22 +1,42 @@
-import {Get, Controller } from '@nestjs/common';
+import { Get, Controller } from "@nestjs/common";
 
-import { TagEntity } from './tag.entity';
-import { TagService } from './tag.service';
+import { TagService } from "./tag.service";
 
 import {
-  ApiBearerAuth, ApiTags,
-} from '@nestjs/swagger';
+  ApiBearerAuth,
+  ApiTags,
+  ApiResponse,
+  ApiOperation,
+} from "@nestjs/swagger";
+import { TagsRO, CreateTagDto } from "./tag.dto";
 
 @ApiBearerAuth()
-@ApiTags('tags')
-@Controller('tags')
+@ApiTags("tags")
+@Controller("tags")
 export class TagController {
-
   constructor(private readonly tagService: TagService) {}
 
   @Get()
-  async findAll(): Promise<TagEntity[]> {
-    return await this.tagService.findAll();
-  }
+  @ApiOperation({
+    summary: "Get All Tags",
+    operationId: "GetTags",
+  })
+  @ApiResponse({
+    type: TagsRO,
+  })
+  async findAll(): Promise<TagsRO> {
+    const _tags = await this.tagService.findAll();
+    const tags: CreateTagDto[] = [];
+    for (let i = 0; i < _tags.length; i++) {
+      const tag: CreateTagDto = {
+        tag: _tags[i].tag,
+      };
+      tags.push(tag);
+    }
 
+    const returns: TagsRO = {
+      tags,
+    };
+    return returns;
+  }
 }
