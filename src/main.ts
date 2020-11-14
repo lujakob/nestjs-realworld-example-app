134c3aee6f29cf2a773556de9497969ebbe2dfd7
Daniel Lim <lahuman@daum.net>
2020-11-15 08:43:20 +0900

    fix : default Port

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/docs', app, document);
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
