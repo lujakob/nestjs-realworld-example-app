9498c6b618adace5c4f5e79cf095e200c47b410c
Andrew Wilson <andrew.wilson-2@manchester.ac.uk>
2021-06-20 18:05:07 +0100

    Performance test against the articles endpoint using locust #4 output of test on the command line

---
+++ tests/performance/locustfile.py
@@ -0,0 +1,44 @@
+import time
+import json
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    def __init__(self, parent):
+        super(QuickstartUser, self).__init__(parent)
+
+        self.headers = {
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+        self.token = ""
+
+    @task(3)
+    def get_articles(self):
+        self.client.get("/api/articles", headers=self.headers)
+
+    @task
+    def create_article(self):
+
+        payload = json.dumps({
+            "article": {
+                "title": "How to train your dragon",
+                "description": "Ever wonder how?",
+                "body": "Very carefully.",
+                "tagList": [
+                    "dragons",
+                    "training"
+                ]
+            }
+        })
+        self.client.post("/api/articles", payload, headers=self.headers)
+
+    def on_start(self):
+        response = self.client.post("/api/users/login", json={"user": {
+            "email": "test@example.com",
+            "password": "testpassword"}}, headers=self.headers)
+
+        self.token = json.loads(response.content)['user']['token']
+        self.headers['Authorization'] = 'Token ' + self.token
