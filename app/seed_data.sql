f2cf05f9e3775a8270f7a3c3fa705c28cd6b4462
Andrew Wilson <andrew.wilson-2@manchester.ac.uk>
2021-06-21 21:58:48 +0100

    add user creation to sql seed script #2

--- app/seed_data.sql
+++ app/seed_data.sql
@@ -1,7 +1,11 @@
+COPY public."user" (id, username, email, bio, image, password) FROM stdin;
+3	tuser	t@example.com			$argon2i$v=19$m=4096,t=3,p=1$hkW9lWIvluDwbjqDDbktiw$/tI+NXp4i+fI4vcDy6rbhvl3GsD4XDjFQfrLHvqfLaU
+\.
+
 COPY public.article (slug, title, description, body, created, updated, "tagList", "favoriteCount", "authorId") FROM stdin;
-how-to-train-your-dragon-rxoz69	How to train your dragon	Ever wonder how?		2021-06-21 10:12:59.798815	2021-06-21 10:12:59.798815	dragons,training	0	1
-how-to-train-your-dragon-2wtpgn	How to train your dragon	Ever wonder how?		2021-06-21 10:13:02.672336	2021-06-21 10:13:02.672336	dragons,training	0	1
-how-to-train-your-dragon-6qshw6	How to train your dragon	Ever wonder how?		2021-06-21 10:13:06.389754	2021-06-21 10:13:06.389754	dragons,training	0	1
-how-to-train-your-dragon-ge1ojt	How to train your dragon	Ever wonder how?		2021-06-21 10:13:37.589353	2021-06-21 10:13:37.589353	dragons,training	0	1
-how-to-train-your-dragon-uyq5qe	How to train your dragon	Ever wonder how?		2021-06-21 10:13:41.40624	2021-06-21 10:13:41.40624	dragons,training	0	1
+how-to-train-your-dragon-rxoz69	How to train your dragon	Ever wonder how?		2021-06-21 10:12:59.798815	2021-06-21 10:12:59.798815	dragons,training	0	3
+how-to-train-your-dragon-2wtpgn	How to train your dragon	Ever wonder how?		2021-06-21 10:13:02.672336	2021-06-21 10:13:02.672336	dragons,training	0	3
+how-to-train-your-dragon-6qshw6	How to train your dragon	Ever wonder how?		2021-06-21 10:13:06.389754	2021-06-21 10:13:06.389754	dragons,training	0	3
+how-to-train-your-dragon-ge1ojt	How to train your dragon	Ever wonder how?		2021-06-21 10:13:37.589353	2021-06-21 10:13:37.589353	dragons,training	0	3
+how-to-train-your-dragon-uyq5qe	How to train your dragon	Ever wonder how?		2021-06-21 10:13:41.40624	2021-06-21 10:13:41.40624	dragons,training	0	3
 \.
