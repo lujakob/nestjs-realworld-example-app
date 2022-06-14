f4f58d5551afd6dbe3a8fdabcecf270ccf56003f
Tomasz Tarando <t.tarando@gmail.com>
2022-06-14 18:52:55 +0200

    changing default port for ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     "type": "postgres",
     "host": process.env.DB_HOST || "localhost",
-    "port": process.env.DB_PORT || 54320,
+    "port": process.env.DB_PORT || 5432,
     "username": process.env.DB_USER || "postgres",
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME || "postgres",
