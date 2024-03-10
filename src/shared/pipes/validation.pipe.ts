fa174fb28fe7b81551ba8e6e4d96d44fbeb528ee
canmertinyo <38213551+canmertinyo@users.noreply.github.com>
2024-03-10 20:51:13 +0300

    refactor: more readable code

--- src/shared/pipes/validation.pipe.ts
+++ src/shared/pipes/validation.pipe.ts
@@ -1,37 +1,36 @@
-import {PipeTransform, ArgumentMetadata, BadRequestException, HttpStatus, Injectable} from '@nestjs/common';
-import { validate } from 'class-validator';
-import { plainToClass } from 'class-transformer';
-import { HttpException } from '@nestjs/common/exceptions/http.exception';
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+} from "@nestjs/common";
+import { ValidationError, validate } from "class-validator";
+import { plainToClass } from "class-transformer";
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
-  async transform(value, metadata: ArgumentMetadata) {
-
+  async transform(value: any, metadata: ArgumentMetadata) {
     if (!value) {
-      throw new BadRequestException('No data submitted');
+      throw new BadRequestException("No data submitted");
     }
 
     const { metatype } = metadata;
+
     if (!metatype || !this.toValidate(metatype)) {
       return value;
     }
     const object = plainToClass(metatype, value);
-    const errors = await validate(object);
-    if (errors.length > 0) {
-      throw new HttpException({message: 'Input data validation failed', errors:  this.buildError(errors)}, HttpStatus.BAD_REQUEST);
-    }
-    return value;
-  }
+    const errors = await validate(object, { whitelist: true });
 
-  private buildError(errors) {
-    const result = {};
-    errors.forEach(el => {
-      let prop = el.property;
-      Object.entries(el.constraints).forEach(constraint => {
-        result[prop + constraint[0]] = `${constraint[1]}`;
-      });
-    });
-    return result;
+    if (errors.length === 0) return value;
+
+    const errMessages = errors.flatMap;
+    (error: ValidationError) => {
+      Object.values(error.constraints);
+    };
+
+    throw new BadRequestException(errMessages);
   }
 
   private toValidate(metatype): boolean {
